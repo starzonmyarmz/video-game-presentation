8f4ebf456225f9f2cb1b468dfc81e2cbae7929f4
Daniel Marino <starzonmyarmz@gmail.com>
2018-10-16 14:02:13 -0400

    in viewport

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+import inViewport from './in-viewport.js';
+
 document.addEventListener('DOMContentLoaded', () => {
+  const sections = document.querySelectorAll('section');
+
   document.querySelector('main').hidden = false;
+
+  sections.forEach((section) => {
+    inViewport(section, { offset: -10 }, function(el) {
+      sections.forEach((s) => { s.classList.remove('in-viewport'); });
+      el.classList.add('in-viewport');
+    });
+  });
 });
